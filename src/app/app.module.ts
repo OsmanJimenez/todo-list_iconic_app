e0fe9a4548d432eb480530cb874c143bbbb2479c
OsmanJimenez <osmanjimenez.dev@gmail.com>
2024-09-29 16:57:51 -0500

    Feat: enable pwa and adjust resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -11,14 +11,25 @@ import { environment } from '../environments/environment';
 import { LocalStorageTaskRepository } from './infrastructure/adapters/local-storage-task.repository';
 import { ENCRYPTION_KEY } from './infrastructure/services/encrypted-storage/encryption-key.token';
 import { TASK_REPOSITORY_TOKEN } from './application/ports/task-repository.token';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: TASK_REPOSITORY_TOKEN, useClass: LocalStorageTaskRepository },
-    { provide: ENCRYPTION_KEY, useValue: 'defaultKey' },
+    { provide: ENCRYPTION_KEY, useValue: environment.encryptionKey },
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideRemoteConfig(() => getRemoteConfig()),
   ],
